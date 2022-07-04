0b7a4b66cbc1d93f36a8bcdb9d6ddf63dd60f4f3
Samuel Ferreira <sam85avila@gmail.com>
2022-07-04 15:39:17 -0300

    Desafio Angular Bootcamp DIO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { BookStoreAppComponent } from './components/book-store-app/book-store-app.component';
+import { FiltersComponent } from './components/book-store-app/filters/filters.component';
+import { ProductListComponent } from './components/book-store-app/product-list/product-list.component';
+import { ProductItemComponent } from './components/book-store-app/product-list/product-item/product-item.component';
+import { BookService } from './components/book-store-app/product-list/product-list.component.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    BookStoreAppComponent,
+    FiltersComponent,
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
